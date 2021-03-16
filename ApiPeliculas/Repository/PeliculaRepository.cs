92bfc26f36db3823129971bd84fd26aea4ffdc68
YeiiMacc <yeiimacc22@outlook.es>
2021-03-15 23:42:21 -0500

    Inicial
    Carga inicial

---
+++ ApiPeliculas/Repository/PeliculaRepository.cs
@@ -0,0 +1,78 @@
+﻿using ApiPeliculas.Data;
+using ApiPeliculas.Models;
+using ApiPeliculas.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPeliculas.Repository
+{
+    public class PeliculaRepository : IPeliculaRepository
+    {
+        private readonly ApplicationDbContext _bd;
+
+        public PeliculaRepository(ApplicationDbContext bd)
+        {
+            _bd = bd;
+        }
+        public bool ActualizarPelicula(Pelicula pelicula)
+        {
+            _bd.Pelicula.Update(pelicula);
+            return Guardar();
+        }
+
+        public bool BorrarPelicula(Pelicula pelicula)
+        {
+            _bd.Pelicula.Remove(pelicula);
+            return Guardar();
+        }
+
+        public IEnumerable<Pelicula> BuscarPelicula(string nombre)
+        {
+            IQueryable<Pelicula> query = _bd.Pelicula;
+            if (!string.IsNullOrEmpty(nombre))
+            {
+                query = query.Where(e => e.Nombre.Contains(nombre) || e.Descripcion.Contains(nombre));
+            }
+            return query.ToList();
+        }
+
+        public bool CrearPelicula(Pelicula pelicula)
+        {
+            _bd.Pelicula.Add(pelicula);
+            return Guardar();
+        }
+
+        public bool ExistePelicula(string nombre)
+        {
+            bool valor = _bd.Categoria.Any(c => c.Nombre.ToLower().Trim() == nombre.ToLower().Trim());
+            return valor; 
+        }
+
+        public bool ExistePelicula(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pelicula GetPelicula(int PeliculaId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Pelicula> GetPeliculas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Pelicula> GetPeliculasEnCategoria(int CatId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Guardar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
