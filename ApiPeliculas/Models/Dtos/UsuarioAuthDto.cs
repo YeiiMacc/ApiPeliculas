02374e30eefbe97914d21dd5f1671b047520eee7
YeiiMacc <yeiimacc22@outlook.es>
2021-03-20 16:52:41 -0500

    Dtos Usuarios

---
+++ ApiPeliculas/Models/Dtos/UsuarioAuthDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPeliculas.Models.Dtos
+{
+    public class UsuarioAuthDto
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El usuario es obliogatorio*")]
+        public string UsuarioA { get; set; }
+        [Required(ErrorMessage = "La contrasena es obliogatoria*")]
+        [StringLength(10, MinimumLength = 4, ErrorMessage = "La contrasena debe contener entre 4 y 10 caracteres")]
+        public string Password { get; set; }
+    }
+}
---
+++ ApiPeliculas/Models/Dtos/UsuarioAuthLoginDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPeliculas.Models.Dtos
+{
+    public class UsuarioAuthLoginDto
+    {
+        [Required(ErrorMessage = "El usuario es obliogatorio*")]
+        public string UsuarioA { get; set; }
+        [Required(ErrorMessage = "La contrasena es obliogatoria*")]
+        public string Password { get; set; }
+    }
+}
