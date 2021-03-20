abbd7335e29db9c1ee55602f9b3d408b398533a5
YeiiMacc <yeiimacc22@outlook.es>
2021-03-20 17:36:39 -0500

    Crear Repositorio e Interfaz Usuario + Implementar Interfaz

---
+++ ApiPeliculas/Repository/UsuarioRepository.cs
@@ -0,0 +1,54 @@
+﻿using ApiPeliculas.Data;
+using ApiPeliculas.Models;
+using ApiPeliculas.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPeliculas.Repository
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly ApplicationDbContext _bd;
+
+        public UsuarioRepository(ApplicationDbContext bd)
+        {
+            _bd = bd;
+        }
+
+        public bool ExisteUsuario(string usuario)
+        {
+            if (_bd.Usuario.Any(x => x.UsuarioA == usuario))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public Usuario GetUsuario(int UsuarioId)
+        {
+            return _bd.Usuario.FirstOrDefault(c => c.Id == UsuarioId);
+        }
+
+        public ICollection<Usuario> GetUsuarios()
+        {
+            return _bd.Usuario.OrderBy(c => c.UsuarioA).ToList();
+        }
+
+        public bool Guardar()
+        {
+            return _bd.SaveChanges() >= 0 ? true : false;
+        }
+
+        public Usuario Login(string usuario, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario Registro(Usuario usuario, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
