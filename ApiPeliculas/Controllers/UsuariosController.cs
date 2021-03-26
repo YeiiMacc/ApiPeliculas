627b9213b92ed617149a4610d708292737758707
YeiiMacc <yeiimacc22@outlook.es>
2021-03-26 10:47:47 -0500

    Crear controlador Usuarios

---
+++ ApiPeliculas/Controllers/UsuariosController.cs
@@ -0,0 +1,56 @@
+﻿using ApiPeliculas.Models;
+using ApiPeliculas.Models.Dtos;
+using ApiPeliculas.Repository.IRepository;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPeliculas.Controllers
+{
+    [Route("api/Usuarios")]
+    [ApiController]
+    public class UsuariosController : Controller
+    {
+        private readonly IUsuarioRepository _userRepo;
+        private readonly IMapper _mapper;
+
+        public UsuariosController(IUsuarioRepository userRepo, IMapper mapper)
+        {
+            _userRepo = userRepo;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetUsuarios()
+        {
+            var listaUsuarios = _userRepo.GetUsuarios();
+            var listaUsuariosDto = new List<UsuarioDto>();
+            foreach (var lista in listaUsuarios)
+            {
+                listaUsuariosDto.Add(_mapper.Map<UsuarioDto>(lista));
+            }
+
+            return Ok(listaUsuariosDto);
+        }
+
+        [HttpGet("{usuarioId:int}", Name = "GetUsuario")]
+        public IActionResult GetUsuario(int UsuarioId)
+        {
+            var itemUsuario = _userRepo.GetUsuario(UsuarioId);
+
+            if (itemUsuario == null)
+            {
+                return NotFound();
+            }
+
+            var itemUsuarioDto = _mapper.Map<UsuarioDto>(itemUsuario);
+            return Ok(itemUsuarioDto);
+        }
+
+        
+    }
+}
